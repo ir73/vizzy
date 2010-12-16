650c4236d764bdcd7077aa390eeca86901a93dff
sergei.ledvanov <sergei.ledvanov@fefc9ee7-b91a-80e9-f855-431138260dec>
2010-12-16 07:10:13 +0000

    added line numbers field

---
+++ trunk/Vizzy/src/vizzy/comp/LineHeightTextArea.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vizzy.comp;
+
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author sergeil
+ */
+public class LineHeightTextArea extends JTextArea {
+
+    public int getRowHeightPubl() {
+        return super.getRowHeight();
+    }
+}
