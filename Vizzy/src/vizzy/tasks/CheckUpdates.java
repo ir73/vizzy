3d43e21096ec7db8c61af55514970eecb7a92b44
sergei.ledvanov <sergei.ledvanov@fefc9ee7-b91a-80e9-f855-431138260dec>
2011-01-17 15:28:54 +0000

    improved filtering usability changed update process to automatic one

--- Vizzy/src/vizzy/tasks/CheckUpdates.java
+++ Vizzy/src/vizzy/tasks/CheckUpdates.java
@@ -8,18 +8,16 @@ import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
-import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import vizzy.listeners.IUpdateCheckListener;
 import vizzy.model.Conf;
-import vizzy.util.DialogUtils;
 
 /**
  *
@@ -29,17 +27,141 @@ public class CheckUpdates extends Thread {
 
     private static final Logger log = Logger.getLogger(CheckUpdates.class);
     
-    private boolean reportIfOk;
+    private boolean isSilent;
     private IUpdateCheckListener listener;
+    private boolean downloaded;
 
-    public CheckUpdates(boolean reportIfOk, IUpdateCheckListener listener) {
-        this.reportIfOk = reportIfOk;
+    public CheckUpdates(IUpdateCheckListener listener) {
+        this.isSilent = true;
         this.listener = listener;
     }
 
     @Override
     public void run() {
-        boolean downloaded = false;
+        downloaded = false;
+
+        try {
+
+
+            String content = getPageContent();
+            if (content == null) {
+                cleanUp();
+                return;
+            }
+
+            String nw = getNewVersion(content);
+            if (nw == null) {
+                cleanUp();
+                return;
+            }
+
+            double newVerd = Double.parseDouble(nw);
+            double verd = Double.parseDouble(Conf.VERSION);
+
+            if (newVerd > verd) {
+
+                File tmpFile = null;
+                try {
+                    tmpFile = downloadNewVersion(nw);
+                } catch (Exception ex) {
+                    log.warn("run() failed to run 'downloadNewVersion'", ex);
+                    showFailedToDownloadAutomatically();
+                }
+
+                if (tmpFile == null) {
+                    cleanUp();
+                    return;
+                }
+
+                String message = null;
+                String newFeatures = null;
+                try {
+                    int i = content.indexOf(Conf.WEBSITE_FEATURES_PHRASE);
+                    int i2 = content.indexOf(";", i);
+                    newFeatures = content.substring(i + Conf.WEBSITE_FEATURES_PHRASE.length(), i2);
+
+                    if (newFeatures == null) {
+                        if (Desktop.isDesktopSupported()) {
+                            try {
+                                Desktop.getDesktop().open(tmpFile);
+                                downloaded = true;
+                            } catch (IOException ex) {
+                            }
+                        }
+                        cleanUp();
+                        return;
+                    }
+                } catch (Exception e) {
+                    log.warn("run() update error 2", e);
+                }
+
+                if (newFeatures == null) {
+                    message = "New version has been downloaded (" + nw + ")!\n"
+                        + "Click OK to install new version.";
+                } else {
+
+                    newFeatures = newFeatures.replaceAll("\\|", "\n");
+
+                    message = "New version has been downloaded (" + nw + ")!\n"
+                        + "Click OK to install new version.\n\n"
+                        + "New features:\n"
+                        + newFeatures;
+                }
+
+                Object[] options = {"OK",
+                    "Cancel",};
+
+                int reply = JOptionPane.showOptionDialog(null, message,
+                        "Update",
+                        JOptionPane.OK_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        options,
+                        options[0]);
+
+                if (reply == JOptionPane.OK_OPTION) {
+                    if (Desktop.isDesktopSupported()) {
+                        try {
+                            Desktop.getDesktop().open(tmpFile);
+                            downloaded = true;
+                        } catch (IOException ex) {
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.warn("run() update error", e);
+        }
+        cleanUp();
+    }
+
+    private File downloadNewVersion(String newVer) throws Exception {
+        String filename = String.format("Vizzy-%s-%s.zip", Conf.OSShortName, newVer);
+        String fileUrl = "http://flash-tracer.googlecode.com/files/" + filename;
+        try {
+
+            byte[] bytes = new byte[1024];
+            URL u = new URL(fileUrl);
+            URLConnection openConnection = u.openConnection();
+            InputStream isr = openConnection.getInputStream();
+
+            File tmpFile = File.createTempFile("Vizzy-" + newVer, ".zip");
+            FileOutputStream fos = new FileOutputStream(tmpFile);
+            int len = 0;
+            while ((len = isr.read(bytes)) != -1) {
+                fos.write(bytes, 0, len);
+            }
+
+            fos.close();
+            isr.close();
+
+            return tmpFile;
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    private String getPageContent() {
         try {
             URL u = new URL(Conf.URL_PROJECT_HOME);
             URLConnection openConnection = u.openConnection();
@@ -57,118 +179,40 @@ public class CheckUpdates extends Thread {
             in.close();
 
             String r = response.toString();
-
-            int i = r.indexOf(Conf.WEBSITE_UPDATE_PHRASE);
-            int i2 = r.indexOf(";", i);
-            if (i > -1) {
-                String newVer = r.substring(i + Conf.WEBSITE_UPDATE_PHRASE.length(), i2);
-                double newVerd = Double.parseDouble(newVer);
-                double verd = Double.parseDouble(Conf.VERSION);
-                if (newVerd > verd) {
-
-                    i = r.indexOf(Conf.WEBSITE_FEATURES_PHRASE);
-                    i2 = r.indexOf(";", i);
-                    String newFeatures = r.substring(i + Conf.WEBSITE_FEATURES_PHRASE.length(), i2);
-                    newFeatures = newFeatures.replaceAll("\\|", "\n");
-
-                    listener.offerUpdate();
-                    
-                    Object[] options = {"Yes",
-                        "No",};
-
-                    int reply = JOptionPane.showOptionDialog(null, "New version is availible (" + newVer + ").\n" +
-                            "Would you like to update now?\n\n" +
-                            "New features:\n" +
-                            newFeatures,
-                            "New Version",
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null,
-                            options,
-                            options[0]);
-                    if (reply == JOptionPane.YES_OPTION) {
-                        downloaded = true;
-                        downloadNewVersion(newVer);
-                    }
-                } else {
-                    if (reportIfOk) {
-                        JOptionPane.showMessageDialog(null, "You have the latest version!", "Info", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                }
-            }
+            return r;
         } catch (Exception ex) {
-            log.warn("run() error", ex);
-        } finally {
-            listener.updateFinished(downloaded);
-            listener = null;
+            log.warn("getNewVersion() error", ex);
         }
-        
+        return null;
     }
 
-    private void downloadNewVersion(String newVer) {
-        String filename = String.format("Vizzy-%s-%s.zip", Conf.OSShortName, newVer);
-//        String filename = String.format("Vizzy_v%s.zip", newVer);
-        String fileUrl = "http://flash-tracer.googlecode.com/files/" + filename;
+    private String getNewVersion(String content) {
+        int i = content.indexOf(Conf.WEBSITE_UPDATE_PHRASE);
+        int i2 = content.indexOf(";", i);
+        if (i > -1) {
+            String newVer = content.substring(i + Conf.WEBSITE_UPDATE_PHRASE.length(), i2);
+            return newVer;
+        }
+        return null;
+    }
+
+    private void showFailedToDownloadAutomatically() {
+        JOptionPane.showMessageDialog(null, "Failed to download update. Please visit product's home page\n"
+                + "and download new version manually.",
+                "Error",
+                JOptionPane.ERROR_MESSAGE);
+
         try {
-
-            DialogUtils.showDialog("Downloading " + filename + "...");
-
-            byte[] bytes = new byte[1024];
-            URL u = new URL(fileUrl);
-            URLConnection openConnection = u.openConnection();
-            InputStream isr = openConnection.getInputStream();
-
-            File tmpFile = File.createTempFile("Vizzy-" + newVer, ".zip");
-            FileOutputStream fos = new FileOutputStream(tmpFile);
-            int len = 0;
-            while ((len = isr.read(bytes)) != -1) {
-                fos.write(bytes, 0, len);
-            }
-
-            fos.close();
-            isr.close();
-
-            File saveFile = new File(filename);
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setSelectedFile(saveFile);
-            DialogUtils.setVisible(false);
-            while (true) {
-                int choice = fileChooser.showSaveDialog(null);
-                if (choice == JFileChooser.APPROVE_OPTION) {
-                    File chosen = fileChooser.getSelectedFile();
-                    saveFile = chosen;
-                    break;
-                } else {
-                    break;
-                }
-            }
-
-            FileUtils.copyFile(tmpFile, saveFile);
-            
             if (Desktop.isDesktopSupported()) {
-                Desktop.getDesktop().open(saveFile);
+                Desktop.getDesktop().browse(new URI(Conf.URL_PROJECT_DOWNLOAD));
             }
+        } catch (Exception ex1) {
+            //                log.warn("downloadNewVersion() desktop not supported", ex);
+        }
+    }
 
-
-            DialogUtils.closeDialog();
-        } catch (Exception ex) {
-            DialogUtils.closeDialog();
-
-            log.warn("downloadNewVersion() failed to download uptdate", ex);
-
-            JOptionPane.showMessageDialog(null, "Failed to download update. Press OK to go\n"
-                    + "to the web-site and to download\n"
-                    + "new version manually!",
-                    "Error",
-                    JOptionPane.ERROR_MESSAGE);
-
-            try {
-                if (Desktop.isDesktopSupported())
-                    Desktop.getDesktop().browse(new URI(Conf.URL_PROJECT_DOWNLOAD));
-            } catch (Exception ex1) {
-//                log.warn("downloadNewVersion() desktop not supported", ex);
-            }
-
-        } 
+    private void cleanUp() {
+        listener.updateFinished(downloaded);
+        listener = null;
     }
 }
